94a612b08087ed3ff560167601e1d448b1524eab
sokunsamnang <sokunsamnang45@gmail.com>
2024-03-14 19:58:59 +0700

    fixed contact

--- components/Phone.tsx
+++ components/Phone.tsx
@@ -9,22 +9,27 @@ const SaveContact: React.FC = () => {
     email: "sokunsamnang45@gmail.com",
   };
 
-  const handleSaveContact = () => {
-    const vCardData = `
-      BEGIN:VCARD
+  const handleCopyContact = () => {
+    const vCardData = `BEGIN:VCARD
       VERSION:3.0
       FN:${contactInfo.name}
       TEL:${contactInfo.phone}
       EMAIL:${contactInfo.email}
-      END:VCARD
-    `;
+      END:VCARD`;
 
-    const dataURI = `data:text/vcard;charset=utf-8,${encodeURIComponent(
-      vCardData
-    )}`;
-
-    // Open the Data URI in the same window to trigger the "Create New Contact" action
-    window.location.href = dataURI;
+    navigator.clipboard
+      .writeText(vCardData)
+      .then(() => {
+        alert(
+          "Contact information copied. Please paste it into your contacts app."
+        );
+      })
+      .catch((error) => {
+        console.error("Error copying contact information:", error);
+        alert(
+          "Failed to copy contact information. Please manually copy and paste it."
+        );
+      });
   };
 
   return (
@@ -32,7 +37,7 @@ const SaveContact: React.FC = () => {
       aria-label="Save Contact"
       type="button"
       className="p-3 mx-1 font-bold capitalize rounded-lg bg-zinc-400 hover:bg-zinc-600"
-      onClick={handleSaveContact}
+      onClick={handleCopyContact}
     >
       <Phone />
     </button>
