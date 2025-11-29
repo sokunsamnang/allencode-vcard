878fcf63a6be7da9476adb06274234fd026b6395
Sam Allen <sokunsamnang45@gmail.com>
2025-11-30 01:38:33 +0700

    fix: Correct import order in layout.tsx and update engines in package.json

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 import StarsCanvas from "@/components/main/StarsBackground";
 import Navbar from "@/components/Navbar";
 
